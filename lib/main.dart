efe205902f1d8645bf60bf7b857650c44ad55bde
Kenny <niall.s.kenny@gmail.com>
2021-10-15 18:13:17 +0100

    move PodcastShowListTile to file

--- lib/main.dart
+++ lib/main.dart
@@ -454,57 +454,6 @@ class _AccountPage extends StatelessWidget {
   }
 }
 
-class PodcastShowListTile extends StatelessWidget {
-  const PodcastShowListTile({
-    Key? key,
-    required this.show,
-    required this.account,
-    required AudioPlayer audioPlayer,
-  })  : _audioPlayer = audioPlayer,
-        super(key: key);
-
-  final PodcastShow show;
-  final Account account;
-  final AudioPlayer _audioPlayer;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () async {
-        await Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute<void>(
-            builder: (BuildContext context) => PodcastShowPage(
-              account: account,
-              show: show,
-              audioPlayer: _audioPlayer,
-            ),
-          ),
-          ModalRoute.withName('/'),
-        );
-      },
-      leading: Hero(
-        tag: "podcast_image" + show.id(),
-        child: Image.network(
-          show.image(),
-          height: 50,
-          width: 50,
-          errorBuilder: (_, __, ___) => const SizedBox.shrink(),
-        ),
-      ),
-      title: Text(
-        show.title(),
-        maxLines: 2,
-      ),
-      subtitle: Text(
-        show.desc(),
-        maxLines: 2,
-        overflow: TextOverflow.ellipsis,
-      ),
-    );
-  }
-}
-
 class PodcastShowPage extends StatefulWidget {
   final PodcastShow show;
   final NetworkImage image;
---
+++ lib/view/utils/tiles/podcasts_show_list_tile.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:fountain_tech_test/data/podcast_index_dot_org.dart';
+import 'package:fountain_tech_test/domain/account.dart';
+import 'package:fountain_tech_test/main.dart';
+
+class PodcastShowListTile extends StatelessWidget {
+  const PodcastShowListTile({
+    Key? key,
+    required this.show,
+    required this.account,
+    required AudioPlayer audioPlayer,
+  })  : _audioPlayer = audioPlayer,
+        super(key: key);
+
+  final PodcastShow show;
+  final Account account;
+  final AudioPlayer _audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () async {
+        await Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute<void>(
+            builder: (BuildContext context) => PodcastShowPage(
+              account: account,
+              show: show,
+              audioPlayer: _audioPlayer,
+            ),
+          ),
+          ModalRoute.withName('/'),
+        );
+      },
+      leading: Hero(
+        tag: "podcast_image" + show.id(),
+        child: Image.network(
+          show.image(),
+          height: 50,
+          width: 50,
+          errorBuilder: (_, __, ___) => const SizedBox.shrink(),
+        ),
+      ),
+      title: Text(
+        show.title(),
+        maxLines: 2,
+      ),
+      subtitle: Text(
+        show.desc(),
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+      ),
+    );
+  }
+}
